9215524010d15ea14aec21c1004bb5ed72d34081
Hollis Jamison <hollisjamison@gmail.com>
2023-04-15 20:09:59 -0400

    Adds intial user object

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: "1",
+    firstName: "Hollis",
+    lastName: "Jamison",
+    username: "hollis_jamison",
+    sin: "555-22-2211",
+    phone: "706271271",
+    gender: "male",
+    city: "Jacksonville",
+    state: "FL",
+    avatar: "https://robohash.org/optiosuscipitperferendis.png",
+  },
+];
