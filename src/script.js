31d1c3ee28fd38e92945dfb418e70afebfcbfadb
Hollis Jamison <hollisjamison@gmail.com>
2023-04-15 20:10:03 -0400

    Adds intial user object

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 const users = [
   {
     id: "1",
-    firstName: "Hollis",
-    lastName: "Jamison",
+    first_name: "Hollis",
+    last_name: "Jamison",
     username: "hollis_jamison",
-    sin: "555-22-2211",
+    social_insurance_number: "555-22-2211",
     phone: "706271271",
     gender: "male",
     city: "Jacksonville",
